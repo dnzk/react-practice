67f60eef8a2ac638bdd1855f9d92de4cb908291c
dnzk <danishkalesaran@gmail.com>
2016-07-23 18:39:38 +0700

    modularize .vote

--- test/core_spec.js
+++ test/core_spec.js
@@ -99,44 +99,32 @@ describe('application logic', () => {
   describe('vote', () => {
     it('creates a tally for the voted entry', () => {
       const state = Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 days later')
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 days later')
       });
       const nextState = vote(state, 'Trainspotting');
       expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 days later'),
-          tally: Map({
-            'Trainspotting': 1
-          })
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 days later'),
+        tally: Map({
+          'Trainspotting': 1
+        })
       }));
     });
 
     it('adds to existing tally for the voted entry', () => {
       const state = Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 days later'),
-          tally: Map({
-            'Trainspotting': 3,
-            '28 days later': 2
-          })
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 days later'),
+        tally: Map({
+          'Trainspotting': 3,
+          '28 days later': 2
+        })
       });
       const nextState = vote(state, 'Trainspotting');
       expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 days later'),
-          tally: Map({
-            'Trainspotting': 4,
-            '28 days later': 2
-          })
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 days later'),
+        tally: Map({
+          'Trainspotting': 4,
+          '28 days later': 2
+        })
       }));
     });
   });
